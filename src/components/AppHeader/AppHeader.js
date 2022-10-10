86a3683c84fa7e07ed9edf4db5bace6fa97aa4b8
harlembeibs <harlembeibs@yandex.ru>
2022-10-11 01:08:46 +0300

    sprint1 version 1

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -6,33 +6,33 @@ import { ListIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import { ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 class AppHeader extends React.Component {
-    render() {
-      return (
-        <header className={AppHeaderStyles.header}>
-					<div className={AppHeaderStyles.container}>
-						<nav className={AppHeaderStyles.menu}>
-							<div className={AppHeaderStyles.menuElem}>
-								<BurgerIcon />
-								<span className={AppHeaderStyles.menuTriggerText}>Конструктор</span>
-							</div>
-							<div className={AppHeaderStyles.menuElem}>
-								<ListIcon type="secondary"/>
-								<span className={AppHeaderStyles.menuTriggerTextDisabled}>Лента заказов</span>
-							</div>
-						</nav>
-						<a className={AppHeaderStyles.logoContainer} href="#">
-							<Logo />
+  render() {
+    return (
+      <header className={AppHeaderStyles.header}>
+				<div className={AppHeaderStyles.container}>
+					<nav className={AppHeaderStyles.menu}>
+						<a href="#" className={AppHeaderStyles.menuElem}>
+							<BurgerIcon />
+							<span className={AppHeaderStyles.menuTriggerText}>Конструктор</span>
 						</a>
-						<nav className={AppHeaderStyles.menu}>
-							<div className={AppHeaderStyles.menuElem}>
-								<ProfileIcon type="secondary" />
-								<span className={AppHeaderStyles.menuTriggerTextDisabled}>Личный кабинет</span>
-							</div>
-						</nav>
-					</div>
-        </header>
-      );
-    }
+						<a href="#" className={AppHeaderStyles.menuElem}>
+							<ListIcon type="secondary"/>
+							<span className={AppHeaderStyles.menuTriggerTextDisabled}>Лента заказов</span>
+						</a>
+					</nav>
+					<a className={AppHeaderStyles.logoContainer} href="#">
+						<Logo />
+					</a>
+					<nav className={AppHeaderStyles.menu}>
+						<a href="#" className={AppHeaderStyles.menuElem}>
+							<ProfileIcon type="secondary" />
+							<span className={AppHeaderStyles.menuTriggerTextDisabled}>Личный кабинет</span>
+						</a>
+					</nav>
+				</div>
+      </header>
+    );
   }
+}
   
-  export default AppHeader;
+export default AppHeader;
--- src/components/BurgerConstructor/BurgerConstructor.js
+++ src/components/BurgerConstructor/BurgerConstructor.js
@@ -8,29 +8,37 @@ import { Scrollbars } from 'react-custom-scrollbars-2';
 
 const BurgerConstructorItems = ({ thread }) => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', gap: '16px' }}>
+    <div className={burgerConstructorStyles.BurgerConstructorItemsContainer}>
       {
         thread.map((item, index) => {
+          if (index !== 0 && index !== thread.length - 1)
           return <ConstructorElement
-            isLocked={index === 0 || index === thread.length - 1 ? true : undefined}
-            type={index === 0 ? "top" : index === thread.length - 1 ? "bottom" : undefined} 
+            isLocked={undefined}
+            type={undefined} 
             text={item.name}
             price={item.price}
             thumbnail={item.image}
-            key={index}
+            key={item._id}
           />
         })}
     </div>
   )
 }
 
+const messagePropTypes = PropTypes.shape({
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired
+});
+
+
 BurgerConstructorItems.propTypes = {
-  image: PropTypes.string,
-  price: PropTypes.string,
-  name: PropTypes.string
+  thread: PropTypes.arrayOf(messagePropTypes).isRequired
 };
 
-const GetTotalPrice = ({ thread }) => {
+
+
+const TotalPrice = ({ thread }) => {
   return (
     thread.reduce((total, value) => {
       return total + value.price
@@ -38,25 +46,51 @@ const GetTotalPrice = ({ thread }) => {
   )
 }
 
-class BurgerConstructor extends React.Component {
-  constructor(props) {
-    super(props)
-  }
+TotalPrice.propTypes = {
+  thread: PropTypes.arrayOf(messagePropTypes).isRequired
+};
 
+class BurgerConstructor extends React.Component {
   render() {
     return (
       <section className={burgerConstructorStyles.container}>
-        <Scrollbars style={{ width: 552, height: 630 }} renderThumbVertical={props => <div {...props} className="thumb-vertical" style={{ backgroundColor: '#8585AD', maxHeight: 664 }}/>}>
+        {
+        this.props.thread.map((item, index) => {
+          if (index === 0) {
+            return <ConstructorElement
+            isLocked={true}
+            type="top" 
+            text={item.name}
+            price={item.price}
+            thumbnail={item.image}
+            key={item._id}
+          />
+          }
+        })}
+        <Scrollbars className={burgerConstructorStyles.scrollBar} renderThumbVertical={props => <div {...props} className={burgerConstructorStyles.thumbVertical} />}>
           <article className={burgerConstructorStyles.itemsContainer}>
             <BurgerConstructorItems thread={this.props.thread} />
           </article>
         </Scrollbars>
+        {
+        this.props.thread.map((item, index) => {
+          if (index === this.props.thread.length - 1) {
+            return <ConstructorElement
+            isLocked={true}
+            type="bottom" 
+            text={item.name}
+            price={item.price}
+            thumbnail={item.image}
+            key={item._id}
+          />
+          }
+        })}
         <div className={burgerConstructorStyles.totalPriceContainer}>
           <div className={burgerConstructorStyles.totalPrice}>
-            <span className={burgerConstructorStyles.totalPriceText}>{<GetTotalPrice thread={this.props.thread} />}</span>
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="#F2F2F3"><path d="M10.3849 2.65561C10.5818 2.18895 10.0397 1.75899 9.63011 2.05689L1.41184 8.03382C1.15309 8.222 1.00001 8.52262 1.00001 8.84256V13.4828C1.00001 13.6932 1.13171 13.8811 1.32948 13.9529L4.15637 14.9785C4.65685 15.1601 5.21185 14.9177 5.41879 14.4271L10.3849 2.65561Z"></path><path d="M1.62116 15.9076C1.32217 15.7916 1.00001 16.0122 1 16.3329C1 16.4889 1.07968 16.634 1.21127 16.7178L10.2307 22.4574C10.3326 22.5223 10.4408 22.3844 10.3536 22.3008L5.22556 17.3879C5.13043 17.2968 5.01823 17.2254 4.89541 17.1777L1.62116 15.9076Z"></path><path d="M13.6465 22.3008C13.5592 22.3844 13.6674 22.5223 13.7693 22.4574L22.7887 16.7178C22.9203 16.634 23 16.4889 23 16.3329C23 16.0122 22.6778 15.7916 22.3788 15.9076L19.1046 17.1777C18.9818 17.2254 18.8696 17.2968 18.7745 17.3879L13.6465 22.3008Z"></path><path d="M22.6705 13.9529C22.8683 13.8811 23 13.6932 23 13.4828V8.84256C23 8.52262 22.8469 8.222 22.5882 8.03382L14.3699 2.05689C13.9603 1.75899 13.4183 2.18895 13.6151 2.65561L18.5812 14.4271C18.7882 14.9177 19.3432 15.1601 19.8436 14.9785L22.6705 13.9529Z"></path><path d="M12.7142 20.9615C12.3221 21.3616 11.6779 21.3616 11.2858 20.9615L7.10635 16.6968C6.83068 16.4155 6.7458 15.9986 6.88954 15.6319L11.069 4.97004C11.4009 4.12332 12.5991 4.12333 12.931 4.97004L17.1105 15.6319C17.2542 15.9986 17.1693 16.4155 16.8937 16.6968L12.7142 20.9615Z"></path></svg>
+            <span className={burgerConstructorStyles.totalPriceText}>{<TotalPrice thread={this.props.thread} />}</span>
+            <svg xmlns="http://www.w3.org/2000/svg" className={burgerConstructorStyles.svg} viewBox="0 0 24 24"><path d="M10.3849 2.65561C10.5818 2.18895 10.0397 1.75899 9.63011 2.05689L1.41184 8.03382C1.15309 8.222 1.00001 8.52262 1.00001 8.84256V13.4828C1.00001 13.6932 1.13171 13.8811 1.32948 13.9529L4.15637 14.9785C4.65685 15.1601 5.21185 14.9177 5.41879 14.4271L10.3849 2.65561Z"></path><path d="M1.62116 15.9076C1.32217 15.7916 1.00001 16.0122 1 16.3329C1 16.4889 1.07968 16.634 1.21127 16.7178L10.2307 22.4574C10.3326 22.5223 10.4408 22.3844 10.3536 22.3008L5.22556 17.3879C5.13043 17.2968 5.01823 17.2254 4.89541 17.1777L1.62116 15.9076Z"></path><path d="M13.6465 22.3008C13.5592 22.3844 13.6674 22.5223 13.7693 22.4574L22.7887 16.7178C22.9203 16.634 23 16.4889 23 16.3329C23 16.0122 22.6778 15.7916 22.3788 15.9076L19.1046 17.1777C18.9818 17.2254 18.8696 17.2968 18.7745 17.3879L13.6465 22.3008Z"></path><path d="M22.6705 13.9529C22.8683 13.8811 23 13.6932 23 13.4828V8.84256C23 8.52262 22.8469 8.222 22.5882 8.03382L14.3699 2.05689C13.9603 1.75899 13.4183 2.18895 13.6151 2.65561L18.5812 14.4271C18.7882 14.9177 19.3432 15.1601 19.8436 14.9785L22.6705 13.9529Z"></path><path d="M12.7142 20.9615C12.3221 21.3616 11.6779 21.3616 11.2858 20.9615L7.10635 16.6968C6.83068 16.4155 6.7458 15.9986 6.88954 15.6319L11.069 4.97004C11.4009 4.12332 12.5991 4.12333 12.931 4.97004L17.1105 15.6319C17.2542 15.9986 17.1693 16.4155 16.8937 16.6968L12.7142 20.9615Z"></path></svg>
           </div>
-          <Button className={burgerConstructorStyles.button}>
+          <Button htmlType="button" className={burgerConstructorStyles.button}>
             Оформить заказ
           </Button>
         </div>
@@ -65,5 +99,8 @@ class BurgerConstructor extends React.Component {
   }
 }
 
+BurgerConstructor.propTypes = {
+  thread: PropTypes.arrayOf(messagePropTypes).isRequired
+};
 
 export default BurgerConstructor;
