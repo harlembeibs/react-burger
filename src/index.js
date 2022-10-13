75ca1343fc0e6783d0114ac3e711ac48e453bd1f
Mansur Rakhimov <93254198+harlembeibs@users.noreply.github.com>
2022-10-14 00:15:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './components/app/app.js';
-import data from '../src/utils/utils.js';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(
-  <React.StrictMode>
-    <App data={data} />
-  </React.StrictMode>
-);
